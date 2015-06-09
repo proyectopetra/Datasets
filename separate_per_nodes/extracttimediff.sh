aa73f11b9a3e787a61fcc38d08a6e038870b9783
fergunet <fergunet@gmail.com>
2015-06-09 17:29:08 +0200

    Borrado el separate per nodes

--- separate_per_nodes/extracttimediff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in `ls *.csv`; do
-salida=`echo $file | tr "." " " | awk '{print $1}'`
-cat $file | tr " " "_" | tr "," " "|grep -v idNodo | awk '{print $5}' > $salida.t
-
-done
